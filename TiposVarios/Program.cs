1bee1ad426a3f333c5f480ec944dd079aa1cc7ec
GenericExcuses <emaildotomasvaz@gmail.com>
2022-02-24 15:44:00 +0000

    TiposVarios

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i variavelBruh = 10;
+            Uint x = 3;
+
+
+
+            Console.WriteLine(i);
+            Console.WriteLine(x);
+        }
+    }
+}
