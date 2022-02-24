dcfca67fbfd81af6f7db943e1734fb76b1811ba9
GenericExcuses <emaildotomasvaz@gmail.com>
2022-02-24 15:20:33 +0000

    Argumentos

---
+++ Argumentos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+
+                string value = args[i];
+                Console.WriteLine("for: {0}", value);
+
+            }       
+        }
+    }
+}
+
