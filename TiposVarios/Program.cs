59d82eab3013d922f8fc2db1f8b99c8a280a394e
GenericExcuses <emaildotomasvaz@gmail.com>
2022-03-01 21:56:23 +0000

    TiposVarios

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -8,12 +8,18 @@ namespace TiposVarios
         {
             int i = 10;
             uint x = 3;
-            char lorem = 'S';
+            char lorem = '\u00A9';
+            float realNumber = 3.14f;
+            bool falseBruh = false;
+            bool trueBruh = true;
 
 
             Console.WriteLine(i);
             Console.WriteLine(x);
             Console.WriteLine(lorem);
+            Console.WriteLine(realNumber);
+            Console.WriteLine(falseBruh);
+            Console.WriteLine(trueBruh);
         }
     }
 }
