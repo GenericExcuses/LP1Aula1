574e2d1b2ddcc5cff94115691b56f4f0222b6c52
GenericExcuses <emaildotomasvaz@gmail.com>
2022-03-21 21:08:56 +0000

    Exercício 6 strings em hexa, percentagens, moedas, verbatim etc

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -8,8 +8,8 @@ namespace VariasStrings
         {
             string x = "Io";
             string y = "Oi"; 
-
-
+            double xx = 0.12345;
+           
 
             string s = "Ola Mundo!";
             Console.WriteLine (s);
@@ -19,8 +19,11 @@ namespace VariasStrings
             string z = "a" + 5;
             string g = "a" + x;
             string h = $"valor de x e {x}";
-            string q = String .Format("Y e {1}, X e {0}", x, y);
-            Console.WriteLine(@"Verbatim com x = {0}", x);
+            string q = String.Format("Y e {1}, X e {0}", x, y);
+            string kk = String.Format("{1,6:f2} e {0:p1}", 0.1234, 18);
+            string k = $"x={xx:f2}";
+
+            string l = String.Format("{1,6:x2} e {0:c1}", 0.1234, 18);
 
             Console.WriteLine (b);
             Console.WriteLine (e);
@@ -28,6 +31,10 @@ namespace VariasStrings
             Console.WriteLine (g);
             Console.WriteLine (h);
             Console.WriteLine (q);
+            Console.WriteLine(@"Verbatim com x = {0}", x);
+            Console.WriteLine(k);
+            Console.WriteLine(kk);
+            Console.WriteLine(l);
         }
     }
 }
